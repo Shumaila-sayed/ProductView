727e14d3a8d8d3cb4fc9846cc6b94aea7691e5e3
Shumaila <sayedshumaila27@gmail.com>
2025-06-28 20:55:53 +0530

    Structure form

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,5 +1,29 @@
 const Form = () => {
-    return <h1>Form element</h1>
-}
+	return (
+		<div className='max-w-min m-auto p-3'>
+			<form className='flex flex-col'>
+				<label htmlFor='user'>
+					Name:
+					<input
+						type='text'
+						name='user'
+						id='user'
+						className='border-gray-800 border-2 rounded-sm'
+					/>
+				</label>
+				<label htmlFor='pwd'>
+					Password:
+					<input
+						type='password'
+						name='pwd'
+						id='pwd'
+						className='border-gray-800 border-2 rounded-sm'
+					/>
+				</label>
+				<button>Login</button>
+			</form>
+		</div>
+	);
+};
 
 export default Form;
